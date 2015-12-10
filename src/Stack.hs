78031f1bdb368087b71ab6dd91542986932190cb
Eric Tsiliacos <erictsiliacos@gmail.com>
2015-12-09 18:22:56 -0800

    Remove comments

--- src/Stack.hs
+++ src/Stack.hs
@@ -5,13 +5,6 @@ data Stack a = Empty | Item
 empty :: Stack a
 empty = Empty
 
--- 'a' is a generic (not special character)
--- 'a' could be Int or String or Bool
--- Stack a versus Stack b
-
--- Stack a is the first parameter
--- mySpecialStack is the paramter
-
 isEmpty :: Stack a -> Bool
 isEmpty Empty = True
 isEmpty _ = False
